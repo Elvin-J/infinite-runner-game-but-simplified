e3ef6b93f9fd3f3bf5c0c812f18ddd79c06a911a
Elvin-J <115949573+Elvin-J@users.noreply.github.com>
2022-12-20 13:55:01 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+
+var obstaclesGroup, obstacle1;
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  
+  
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided" , trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  console.log("Hello" + 5);
+  
+  score = 0;
+}
+
+function draw() {
+  background(180);
+  text("Score: "+ score, 500,50);
+  score = score + Math.round(frameCount/60);
+  
+  
+  if(keyDown("up_arrow")&& trex.y >= 100) {
+    trex.velocityY = -13;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  
+  
+  //spawn obstacles on the ground
+  spawnObstacles();
+  
+  drawSprites();
+}
+
+function spawnObstacles(){
+ if (frameCount % 60 === 0){
+   var obstacle = createSprite(400,165,10,40);
+   obstacle.velocityX = -6;
+
+   
+    // //generate random obstacles
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      
+    }
+   
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+ }
+}
+
+
+
+
